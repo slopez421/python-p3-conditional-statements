371c629cbeaebdcf29d3f9ac506b8c13e7a69d6f
Shania Lopez <slopez@fandm.edu>
2024-04-26 08:07:12 -0400

    lab done

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,42 +1,41 @@
-#!/usr/bin/env python3
-
 def admin_login(username, password):
-    if (username == "admin" and password == "12345"):
-        return "Access granted"
-    elif (username == "ADMIN" and password == "12345"):
-        return "Access granted"
+    if username == "admin" and password == "12345":
+            return "Access granted"
+    elif username == "ADMIN" and password == "12345":
+            return "Access granted"
     else:
         return "Access denied"
 
 def hows_the_weather(temperature):
-    if (temperature > 85):
-        return "It's too dang hot out there!"
-    elif (temperature < 40): 
-        return "It's brisk out there!"
-    elif(temperature <= 65):
-        return "It's a little chilly out there!"
-    else: 
-        return "It's perfect out there!"
+    if temperature < 40:
+         return "It's brisk out there!"
+    elif 65 > temperature > 40:
+         return "It's a little chilly out there!"
+    elif temperature > 85:
+         return "It's too dang hot out there!"
+    else:
+         return "It's perfect out there!"
+    pass
 
 def fizzbuzz(num):
-    if (num % 3 == 0 and num % 5 == 0):
-        return "FizzBuzz"
-    elif (num % 3 == 0):
-        return "Fizz"
-    elif (num % 5 == 0):
-        return "Buzz"
+    if (num  % 5 == 0 and num % 3 == 0):
+         return "FizzBuzz"
+    elif num % 3 == 0:
+         return "Fizz"
+    elif num % 5 == 0:
+         return "Buzz"
     else:
-        return num
+         return num
 
 def calculator(operation, num1, num2):
-    if (operation == "+"):
-        return num1 + num2
-    elif (operation == "-"):
-        return num1 - num2
-    elif (operation == "/"):
-        return num1 / num2
-    elif (operation == "*"):
-        return num1 * num2
+    if operation == "+":
+         return num1 + num2
+    elif operation == "-":
+         return num1 - num2
+    elif operation == "*":
+         return num1 * num2
+    elif operation == "/":
+         return num1 / num2
     else:
-        print("Invalid operation!")
-        return None
+         print("Invalid operation!")
+         return None
